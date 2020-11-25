5f0de456649c6aa3998928ad315c1eb4f286cd20
Nico Cobelo <ingcobelo@gmail.com>
2020-11-25 19:14:57 -0300

    Add Data Manipulation, New Categories

--- shoppingList/Controller/ListViewController.swift
+++ shoppingList/Controller/ListViewController.swift
@@ -6,14 +6,18 @@
 //
 
 import UIKit
+import CoreData
 
 class ListViewController: UITableViewController {
 
     var  categories = [Category]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        loadCategories()
     }
 
     // MARK: - Tableview Datasoruce Methods
@@ -37,10 +41,48 @@ class ListViewController: UITableViewController {
     
     //MARK: - Data Manipulation
     
+    func saveCategories() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadCategories(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        do {
+            categories = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+    }
+    
     //MARK: - Add New Categories
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Create New Category", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            let newCategory = Category(context: self.context)
+            newCategory.name = textField.text
+            
+            self.categories.append(newCategory)
+            self.saveCategories()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new category"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
     }
     //MARK: - Tableview Delegate Methods
 }
